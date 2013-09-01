d808332745534d8059e2790a8ca2f3495d85458b
FreeBirdLjj <ljj11011@mail.ustc.edu.cn>
2013-09-01 15:38:54 +0800

    First commit

---
+++ main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: main.sh
+# 
+#         USAGE: ./main.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 09/01/2013 15:35
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+echo "Please input the email you register to Renren:"
+read email
+echo "Please input your Renren password(without echo):"
+read -s password
+curl -c cookie -d "email=${email}&password=${password}" http://www.renren.com/PLogin.do
+curl -L -b cookie http://www.renren.com/home
+
